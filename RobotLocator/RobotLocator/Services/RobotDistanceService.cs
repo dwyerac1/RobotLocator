ba7fc19ed13230123ae5178166c3f22f503182ed
Anthony Dwyer <dwyerac1@gmail.com>
2021-12-01 17:54:48 -0500

    Calculate Robot Distance functionality initial commit - need to test and validate

--- RobotLocator/RobotLocator/Services/RobotDistanceService.cs
+++ RobotLocator/RobotLocator/Services/RobotDistanceService.cs
@@ -5,9 +5,42 @@ namespace RobotLocator.Services
 {
     public class RobotDistanceService : IRobotDistanceService
     {
+
+        private readonly IDistanceCalculatorService _distanceCalculatorService;
+
+        public RobotDistanceService(IDistanceCalculatorService distanceCalculatorService)
+        {
+            _distanceCalculatorService = distanceCalculatorService;
+        }
+
         public RobotDistance GetClosestRobot(Load load, IEnumerable<Robot> robots, int distanceThreshold = 10)
         {
-            throw new NotImplementedException();
+            double? shortestDistance = null;
+            RobotDistance closestRobotDistance = null;
+            foreach (var robot in robots)
+            {
+                var distance = this._distanceCalculatorService.CalculateDistance(load, robot);
+                var robotDistance = new RobotDistance { BatteryLevel = robot.BatteryLevel, DistanceToGoal = distance, RobotId = robot.RobotId };
+                var hasHigherBatteryLevel = false;
+                if (closestRobotDistance == null) // First robot will always be closest if there's one robot
+                {
+                    closestRobotDistance = robotDistance;
+                    shortestDistance = distance;
+                }
+                else
+                {
+                    if (distance <= distanceThreshold && closestRobotDistance.BatteryLevel < robot.BatteryLevel)
+                    {
+                        hasHigherBatteryLevel = true;
+                    }
+                    if (shortestDistance > distance || hasHigherBatteryLevel)
+                    {
+                        shortestDistance = distance;
+                        closestRobotDistance = robotDistance;
+                    }
+                }
+            }
+            return closestRobotDistance;
         }
     }
 }
