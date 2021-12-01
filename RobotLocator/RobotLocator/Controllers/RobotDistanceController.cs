ecf5273965b80efe998bd7838507e0e9358faa67
Anthony Dwyer <dwyerac1@gmail.com>
2021-12-01 17:41:27 -0500

    Inject services and use correct POST method

--- RobotLocator/RobotLocator/Controllers/RobotDistanceController.cs
+++ RobotLocator/RobotLocator/Controllers/RobotDistanceController.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using RobotLocator.DomainModels;
+using RobotLocator.Interfaces;
+using System.Net;
 
 namespace RobotLocator.Controllers
 {
@@ -7,11 +9,28 @@ namespace RobotLocator.Controllers
     [Route("api/[controller]")]
     public class RobotDistanceController : ControllerBase
     {
+        private readonly IRobotDistanceService _robotDistanceService;
+        // Should these be configuration values?
+        private readonly string _externalUrl = "https://60c8ed887dafc90017ffbd56.mockapi.io/robots";
+        private readonly string _altExternalUrl = "https://svtrobotics.free.beeceptor.com/robots";
+        public RobotDistanceController(IRobotDistanceService robotDistanceService)
+        {
+            _robotDistanceService = robotDistanceService;
+        }
         // POST: api/RobotDistance
         [HttpPost]
         public RobotDistance CalculateRobotDistance(Load load)
         {
-            return null;
+            var robots = GetRobotExternalData(_externalUrl, _altExternalUrl);
+            var robotDistance = this._robotDistanceService.GetClosestRobot(load, robots);
+            return robotDistance;
+        }
+        //TODO: See about using http client instead, but don't have time
+        private static IEnumerable<Robot> GetRobotExternalData(string mainUrl, string altUrl)
+        {
+            // Call the external website and populate the robot list
+            return new List<Robot>();
+
         }
     }
 }
